0183bda9637a3af1691070e55df12b0a74e4ddea
Brennan Saeta <brennan.saeta@gmail.com>
2016-05-27 09:58:28 -0700

    Add combinators to StructuredAccessControl
    To provide the maximal flexibility for end users, StructuredAccessControl
    objects should be combine-able in a variety of ways in order to reasonably
    define new policies.
    
    While it is recommended for common constraints to use an AuthenticationCombiner
    to smoothly define a new access control policy, there are a number of cases were
    a more ad-hoc approach is justified.
    
    In order to maintain composibility, we ensure to always return another
    StructuredAccessControl. Note: in order to get appropriate semantics, in some
    combinators, we need to run the Authorizer within the authenticator itself.
    
    Because this is very important code, I've included a comprehensive test suite to
    ensure the correct operation of the combinators.

--- naptime/src/main/scala/org/coursera/naptime/access/StructuredAccessControl.scala
+++ naptime/src/main/scala/org/coursera/naptime/access/StructuredAccessControl.scala
@@ -18,6 +18,8 @@ package org.coursera.naptime.access
 
 import org.coursera.naptime.NaptimeActionException
 import org.coursera.naptime.access.authenticator.Authenticator
+import org.coursera.naptime.access.authenticator.HeaderAuthenticationParser
+import org.coursera.naptime.access.authorizer.AuthorizeResult
 import org.coursera.naptime.access.authorizer.Authorizer
 import play.api.http.Status
 import play.api.mvc.RequestHeader
@@ -44,8 +46,183 @@ case class StructuredAccessControl[A](
           Authorizer.toResponse(authorizer.authorize(decorated), decorated)
         }
       }.getOrElse {
-        Left(NaptimeActionException(Status.UNAUTHORIZED, Some("auth.perms"), Some("Missing authentication")))
+        StructuredAccessControl.missingResponse
       }
     }
+  }
+}
+
+object StructuredAccessControl extends StructuredAccessControlCombinators {
+  private[access] val missingResponse =
+    Left(NaptimeActionException(Status.UNAUTHORIZED, Some("auth.perms"), Some("Missing authentication")))
+}
+
+trait StructuredAccessControlCombinators {
+  /**
+   * A left-biased combinator that allows the request if either the left or the right access
+   * control allow it.
+   *
+   * Example usage:
+   * {{{
+   *   Nap.auth(
+   *     StructuredAccessControl.either(Auth.superuser, Auth.admin)).get(ctx => ???)
+   * }}}
+   *
+   * If there is only one parsing error, it is ignored. If there are two, the right one masks the
+   * left one.
+   */
+  def either[A, B](
+      left: StructuredAccessControl[A],
+      right: StructuredAccessControl[B]): StructuredAccessControl[Either[A, B]] = {
+    val authn = new Authenticator[Either[A, B]] {
+      override def maybeAuthenticate(
+          requestHeader: RequestHeader)
+          (implicit ec: ExecutionContext): Future[Option[Either[NaptimeActionException, Either[A, B]]]] = {
+        val lhsF = left.authenticator.maybeAuthenticate(requestHeader)
+        val rhsF = right.authenticator.maybeAuthenticate(requestHeader)
+        for {
+          lhs <- lhsF
+          rhs <- rhsF
+        } yield {
+          if (lhs.isEmpty && rhs.isEmpty) {
+            Option(StructuredAccessControl.missingResponse)
+          } else {
+            /**
+             * Takes the result of an authenticator, filters it through the corresponding authorizer, and
+             * converts it to the appropriate Either type.
+             */
+            def filterResponse[T](
+                authResult: Option[Either[NaptimeActionException, T]],
+                authorizer: Authorizer[T])
+                (fn: T => Either[A, B]): Either[NaptimeActionException, Either[A, B]] = {
+              authResult.map { authEither =>
+                authEither.right.flatMap { decorated =>
+                  Authorizer.toResponse(authorizer.authorize(decorated), fn(decorated))
+                }
+              }.getOrElse {
+                StructuredAccessControl.missingResponse
+              }
+            }
+            val leftSide = filterResponse(lhs, left.authorizer)(Left.apply)
+            Some(leftSide.left.flatMap { failure =>
+              filterResponse(rhs, right.authorizer)(Right.apply)
+            })
+          }
+        }
+      }
+    }
+    StructuredAccessControl(
+      authenticator = authn,
+      authorizer = Authorizer(_ => AuthorizeResult.Authorized))
+  }
+
+  /**
+   * Allows the request through iff both left and right allow it.
+   *
+   * Example usage:
+   * {{{
+   *   Nap.auth(
+   *     StructuredAccessControl.and(Auth.superuser, Auth.admin)).get(ctx => ???)
+   * }}}
+   *
+   * If there is only one parsing error, it is ignored. If there are two, the left one masks the
+   * right one.
+   */
+  def and[A, B](
+      left: StructuredAccessControl[A],
+      right: StructuredAccessControl[B]): StructuredAccessControl[(A, B)] = {
+    val authn = new Authenticator[(A, B)] {
+      override def maybeAuthenticate(
+        requestHeader: RequestHeader)
+        (implicit ec: ExecutionContext):
+      Future[Option[Either[NaptimeActionException, (A, B)]]] = {
+        val lhsF = left.authenticator.maybeAuthenticate(requestHeader)
+        val rhsF = right.authenticator.maybeAuthenticate(requestHeader)
+        for {
+          lhs <- lhsF
+          rhs <- rhsF
+        } yield {
+          for {
+            lh <- lhs
+            rh <- rhs
+          } yield {
+            for {
+              l <- lh.right
+              r <- rh.right
+            } yield {
+              (l, r)
+            }
+          }
+        }
+      }
+    }
+    val authz = Authorizer.apply[(A, B)] { pair =>
+      val lhs = left.authorizer.authorize(pair._1)
+      val rhs = right.authorizer.authorize(pair._2)
+      if (lhs.isAuthorized && rhs.isAuthorized) {
+        AuthorizeResult.Authorized
+      } else if (lhs.isRejected) {
+        lhs
+      } else if (rhs.isRejected) {
+        rhs
+      } else if (lhs.isFailed) {
+        lhs
+      } else if (rhs.isFailed) {
+        rhs
+      } else {
+        lhs
+      }
+    }
+    StructuredAccessControl(authn, authz)
+  }
+
+  /**
+   * Allows the request through as long as at least one of Access Controllers would allow the request.
+   *
+   * The parse results are exposed to user code as a tuple containing options. At least one of the options
+   * must be a [[Some]].
+   */
+  def anyOf[A, B](
+      left: StructuredAccessControl[A],
+      right: StructuredAccessControl[B]): StructuredAccessControl[(Option[A], Option[B])] = {
+    val authn = new Authenticator[(Option[A], Option[B])] {
+      override def maybeAuthenticate(
+          requestHeader: RequestHeader)
+          (implicit ec: ExecutionContext):
+          Future[Option[Either[NaptimeActionException, (Option[A], Option[B])]]] = {
+        val lhsF = left.authenticator.maybeAuthenticate(requestHeader)
+        val rhsF = right.authenticator.maybeAuthenticate(requestHeader)
+        for {
+          lhs <- lhsF
+          rhs <- rhsF
+        } yield {
+          if (lhs.isEmpty && rhs.isEmpty) {
+            None
+          } else {
+            def filterResponse[T](
+              authResult: Option[Either[NaptimeActionException, T]],
+              authorizer: Authorizer[T]): Either[NaptimeActionException, T] = {
+              authResult.map { authEither =>
+                authEither.right.flatMap { decorated =>
+                  Authorizer.toResponse(authorizer.authorize(decorated), decorated)
+                }
+              }.getOrElse(StructuredAccessControl.missingResponse)
+            }
+            val lh = filterResponse(lhs, left.authorizer)
+            val rh = filterResponse(rhs, right.authorizer)
+            if (lh.isLeft && rh.isLeft) {
+              // Neither of them work, so serve an error response
+              Some(lh.asInstanceOf[Left[NaptimeActionException, Nothing]])
+            } else {
+              // At least one of them is good, so carry on.
+              val l = lh.right.toOption
+              val r = rh.right.toOption
+              Some(Right(l, r))
+            }
+          }
+        }
+      }
+    }
+    StructuredAccessControl(authn, Authorizer(_ => AuthorizeResult.Authorized))
   }
 }
---
+++ naptime/src/test/scala/org/coursera/naptime/access/StructuredAccessControlCombinatorsTest.scala
@@ -0,0 +1,319 @@
+package org.coursera.naptime.access
+
+import org.coursera.naptime.NaptimeActionException
+import org.coursera.naptime.access.authenticator.Authenticator
+import org.coursera.naptime.access.authenticator.Decorator
+import org.coursera.naptime.access.authenticator.HeaderAuthenticationParser
+import org.coursera.naptime.access.authenticator.ParseResult
+import org.coursera.naptime.access.authorizer.AuthorizeResult
+import org.coursera.naptime.access.authorizer.Authorizer
+import org.junit.Ignore
+import org.junit.Test
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.junit.AssertionsForJUnit
+import play.api.http.Status
+import play.api.mvc.RequestHeader
+import play.api.test.FakeRequest
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+class StructuredAccessControlCombinatorsTest extends AssertionsForJUnit with ScalaFutures {
+  import StructuredAccessControlCombinatorsTest._
+
+  def runEither(
+      left: StructuredAccessControl[String] = LEFT,
+      right: StructuredAccessControl[String] = RIGHT)
+      (checks: Either[NaptimeActionException, Either[String, String]] => Unit): Unit = {
+    val either = StructuredAccessControl.either(left, right)
+    val result = either.run(FakeRequest())
+    checks(result.futureValue)
+  }
+
+  def runAnyOf(
+      left: StructuredAccessControl[String] = LEFT,
+      right: StructuredAccessControl[String] = RIGHT)
+      (checks: Either[NaptimeActionException, (Option[String], Option[String])] => Unit): Unit = {
+    val anyOf = StructuredAccessControl.anyOf(left, right)
+    val result = anyOf.run(FakeRequest())
+    checks(result.futureValue)
+  }
+
+  def runAnd(
+    left: StructuredAccessControl[String] = LEFT,
+    right: StructuredAccessControl[String] = RIGHT)
+    (checks: Either[NaptimeActionException, (String, String)] => Unit): Unit = {
+    val and = StructuredAccessControl.and(left, right)
+    val result = and.run(FakeRequest())
+    checks(result.futureValue)
+  }
+
+  @Test
+  def eitherSimple(): Unit = {
+    runEither() { result =>
+      assert(Right(Left("left")) === result)
+    }
+  }
+
+  @Test
+  def eitherFallbackToRightParseMissing(): Unit = {
+    val left = StructuredAccessControl(Authenticators.parseMissing[String], Authorizers.allowed)
+    runEither(left) { result =>
+      assert(Right(Right("right")) === result)
+    }
+  }
+
+  @Test
+  def eitherFallbackToRightParseError(): Unit = {
+    val left = StructuredAccessControl(Authenticators.parseError[String](), Authorizers.allowed)
+    runEither(left) { result =>
+      assert(Right(Right("right")) === result)
+    }
+  }
+
+  @Test
+  def eitherFallbackToRightUnauthorized(): Unit = {
+    val left = StructuredAccessControl(Authenticators.constant("left"), Authorizers.deny())
+    runEither(left) { result =>
+      assert(Right(Right("right")) === result)
+    }
+  }
+
+  @Test
+  def eitherFallbackToRightError(): Unit = {
+    val left = StructuredAccessControl(Authenticators.constant("left"), Authorizers.fail())
+    runEither(left) { result =>
+      assert(Right(Right("right")) === result)
+    }
+  }
+
+  @Test
+  def eitherIgnoreRightParseMissing(): Unit = {
+    val right = StructuredAccessControl(Authenticators.parseMissing[String], Authorizers.allowed)
+    runEither(right = right) { result =>
+      assert(Right(Left("left")) === result)
+    }
+  }
+
+  @Test
+  def eitherIgnoreRightParseError(): Unit = {
+    val right = StructuredAccessControl(Authenticators.parseError[String](), Authorizers.allowed)
+    runEither(right = right) { result =>
+      assert(Right(Left("left")) === result)
+    }
+  }
+
+  @Test
+  def eitherIgnoreRightUnauthorized(): Unit = {
+    val right = StructuredAccessControl(Authenticators.constant("right"), Authorizers.deny())
+    runEither(right = right) { result =>
+      assert(Right(Left("left")) === result)
+    }
+  }
+
+  @Test
+  def eitherIgnoreRightError(): Unit = {
+    val right = StructuredAccessControl(Authenticators.constant("right"), Authorizers.fail())
+    runEither(right = right) { result =>
+      assert(Right(Left("left")) === result)
+    }
+  }
+
+  @Test
+  def eitherBothMissing(): Unit = {
+    val left = StructuredAccessControl(Authenticators.parseMissing[String], Authorizers.allowed)
+    val right = StructuredAccessControl(Authenticators.parseMissing[String], Authorizers.deny())
+    runEither(left, right) { result =>
+      assert(result.isLeft)
+      assert(result.left.get.httpCode === Status.UNAUTHORIZED)
+    }
+  }
+
+  @Test
+  def eitherBothDenied(): Unit = {
+    val left = StructuredAccessControl(Authenticators.constant("left"), Authorizers.deny())
+    val right = StructuredAccessControl(Authenticators.constant("right"), Authorizers.deny())
+    runEither(left, right) { result =>
+      assert(result.isLeft)
+      assert(result.left.get.httpCode === Status.FORBIDDEN)
+    }
+  }
+
+  @Test
+  def anyOfSimple(): Unit = {
+    runAnyOf() { result =>
+      assert(Right((Some("left"), Some("right"))) === result)
+    }
+  }
+
+  @Test
+  def anyOfLeftMissing(): Unit = {
+    val left = StructuredAccessControl(Authenticators.parseMissing[String], Authorizers.allowed)
+    runAnyOf(left) { result =>
+      assert(Right((None, Some("right"))) === result)
+    }
+  }
+
+  @Test
+  def anyOfLeftParseError(): Unit = {
+    val left = StructuredAccessControl(Authenticators.parseError[String](), Authorizers.allowed)
+    runAnyOf(left) { result =>
+      assert(Right((None, Some("right"))) === result)
+    }
+  }
+
+  @Test
+  def anyOfLeftDeny(): Unit = {
+    val left = StructuredAccessControl(Authenticators.constant("left"), Authorizers.deny())
+    runAnyOf(left) { result =>
+      assert(Right((None, Some("right"))) === result)
+    }
+  }
+
+  @Test
+  def anyOfRightDeny(): Unit = {
+    val right = StructuredAccessControl(Authenticators.constant("right"), Authorizers.deny())
+    runAnyOf(right = right) { result =>
+      assert(Right((Some("left"), None)) === result)
+    }
+  }
+
+  @Test
+  def anyOfBothDeny(): Unit = {
+    val left = StructuredAccessControl(Authenticators.constant("left"), Authorizers.deny())
+    val right = StructuredAccessControl(Authenticators.constant("right"), Authorizers.deny())
+    runAnyOf(left, right) { result =>
+      assert(result.isLeft)
+      assert(result.left.get.httpCode === Status.FORBIDDEN)
+    }
+  }
+
+  @Test
+  def anyOfBothSkip(): Unit = {
+    val left = StructuredAccessControl(Authenticators.parseMissing[String], Authorizers.allowed)
+    val right = StructuredAccessControl(Authenticators.parseMissing[String], Authorizers.allowed)
+    runAnyOf(left, right) { result =>
+      assert(result.isLeft)
+      assert(result.left.get.httpCode === Status.UNAUTHORIZED)
+    }
+  }
+
+  @Test
+  def anyOfFailAndSkip(): Unit = {
+    val left = StructuredAccessControl(Authenticators.constant("left"), Authorizers.fail())
+    val right = StructuredAccessControl(Authenticators.constant("right"), Authorizers.deny())
+    runAnyOf(left, right) { result =>
+      assert(result.isLeft)
+      // Behavior is unspecified as to the response code returned.
+    }
+  }
+
+  @Test
+  def anyOfParseErrorAndDeny(): Unit = {
+    val left = StructuredAccessControl(Authenticators.parseError[String](), Authorizers.allowed)
+    val right = StructuredAccessControl(Authenticators.constant("right"), Authorizers.deny())
+    runAnyOf(left, right) { result =>
+      assert(result.isLeft)
+      // Behavior is unspecified as to the response code.
+    }
+  }
+
+  @Test
+  def andSimple(): Unit = {
+    runAnd() { result =>
+      assert(Right("left", "right") === result)
+    }
+  }
+
+  @Test
+  def andLeftDeny(): Unit = {
+    val left = StructuredAccessControl(Authenticators.constant("left"), Authorizers.deny())
+    runAnd(left) { result =>
+      assert(result.isLeft)
+      assert(result.left.get.httpCode === Status.FORBIDDEN)
+    }
+  }
+
+  @Test
+  def andRightDeny(): Unit = {
+    val right = StructuredAccessControl(Authenticators.constant("right"), Authorizers.deny())
+    runAnd(right = right) { result =>
+      assert(result.isLeft)
+      assert(result.left.get.httpCode === Status.FORBIDDEN)
+    }
+  }
+
+  @Test
+  def andBothDeny(): Unit = {
+    val left = StructuredAccessControl(Authenticators.constant("left"), Authorizers.deny())
+    val right = StructuredAccessControl(Authenticators.constant("right"), Authorizers.deny())
+    runAnd(left, right) { result =>
+      assert(result.isLeft)
+      assert(result.left.get.httpCode === Status.FORBIDDEN)
+    }
+  }
+
+  @Test
+  def andDenyAndFail(): Unit = {
+    val left = StructuredAccessControl(Authenticators.constant("left"), Authorizers.fail())
+    val right = StructuredAccessControl(Authenticators.constant("right"), Authorizers.deny())
+    runAnd(left, right) { result =>
+      assert(result.isLeft)
+      assert(result.left.get.httpCode === Status.FORBIDDEN)
+    }
+  }
+
+  @Test
+  def andBothFail(): Unit = {
+    val left = StructuredAccessControl(Authenticators.constant("left"), Authorizers.fail())
+    val right = StructuredAccessControl(Authenticators.constant("right"), Authorizers.fail())
+    runAnd(left, right) { result =>
+      assert(result.isLeft)
+      assert(result.left.get.httpCode === Status.INTERNAL_SERVER_ERROR)
+    }
+  }
+}
+
+object StructuredAccessControlCombinatorsTest {
+  val LEFT = StructuredAccessControl(Authenticators.constant("left"), Authorizers.allowed)
+  val RIGHT = StructuredAccessControl(Authenticators.constant("right"), Authorizers.allowed)
+
+  object Authenticators {
+    def constant[T](constant: T): Authenticator[T] = {
+      Authenticator(HeaderAuthenticationParser.constant(constant), Decorator.identity[T])
+    }
+
+    def parseError[T](
+        errorMsg: String = "parse error!",
+        code: Int = Status.UNAUTHORIZED): Authenticator[T] = {
+      val headerParser = new HeaderAuthenticationParser[T] {
+        override def parseHeader(requestHeader: RequestHeader): ParseResult[T] = {
+          ParseResult.Error(errorMsg)
+        }
+      }
+      Authenticator(headerParser, Decorator.identity[T])
+    }
+
+    def parseMissing[T]: Authenticator[T] = {
+      val headerParser = new HeaderAuthenticationParser[T] {
+        override def parseHeader(requestHeader: RequestHeader): ParseResult[T] = {
+          ParseResult.Skip
+        }
+      }
+      Authenticator(headerParser, Decorator.identity[T])
+    }
+  }
+
+  object Authorizers {
+    def allowed[T]: Authorizer[T] = {
+      Authorizer(_ => AuthorizeResult.Authorized)
+    }
+
+    def deny[T](msg: String = "denied!"): Authorizer[T] = {
+      Authorizer(_ => AuthorizeResult.Rejected(msg))
+    }
+
+    def fail[T](msg: String = "failed!"): Authorizer[T] = {
+      Authorizer(_ => AuthorizeResult.Failed(msg))
+    }
+  }
+}
