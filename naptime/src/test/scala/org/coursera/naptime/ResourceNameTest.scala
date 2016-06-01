2e82af02609f1eadf8e0e229ef24786e30b63538
Brennan Saeta <brennan.saeta@gmail.com>
2016-06-01 15:33:47 -0700

    Migrate over internal model tests to OSS
    As part of removing the internal copy of naptime from Coursera's source tree,
    I discovered these tests had gotten forgotten.

---
+++ naptime/src/test/scala/org/coursera/naptime/ResourceNameTest.scala
@@ -0,0 +1,39 @@
+package org.coursera.naptime
+
+import org.junit.Test
+import org.scalatest.junit.AssertionsForJUnit
+
+class ResourceNameTest extends AssertionsForJUnit {
+
+  @Test
+  def simpleResourceNameIdentifier(): Unit = {
+    assert(ResourceName("foo", 1).identifier === "foo.v1")
+  }
+
+  @Test
+  def nestedResourceNameIdentifier(): Unit = {
+    assert(ResourceName("fooBar", 1, List("history")).identifier === "fooBar.v1/history")
+  }
+
+  @Test
+  def deeplyNestedResourceNameIdenfier(): Unit = {
+    assert(ResourceName("fooBarBaz", 103, List("history", "author")).identifier ===
+      "fooBarBaz.v103/history/author")
+  }
+
+  @Test
+  def parseSimple(): Unit = {
+    assert(ResourceName("foo", 1) === ResourceName.parse("foo.v1").get)
+  }
+
+  @Test
+  def parseNested(): Unit = {
+    assert(ResourceName("fooBar", 2, List("sub")) === ResourceName.parse("fooBar.v2/sub").get)
+  }
+
+  @Test
+  def parseDeeplyNested(): Unit = {
+    assert(ResourceName("fooBar", 3, List("sub", "superSub")) ===
+      ResourceName.parse("fooBar.v3/sub/superSub").get)
+  }
+}
---
+++ naptime/src/test/scala/org/coursera/naptime/RestContextTest.scala
@@ -0,0 +1,54 @@
+package org.coursera.naptime
+
+import org.junit.Test
+import play.api.i18n.Lang
+import play.api.mvc.Request
+import org.mockito.Mockito.when
+import org.scalatest.junit.AssertionsForJUnit
+import org.scalatest.mock.MockitoSugar
+
+class RestContextTest extends AssertionsForJUnit with MockitoSugar {
+
+  private[this] def makeContext(languagePreferences: Seq[Lang]): RestContext[Unit, Unit] = {
+    val mockRequest = mock[Request[Unit]]
+    val restContext = new RestContext((), (), mockRequest, null, null, null)
+    when(mockRequest.acceptLanguages).thenReturn(languagePreferences)
+    restContext
+  }
+
+  def test(
+      requestLanguages: Seq[Lang],
+      availableLanguages: Set[Lang],
+      defaultLanguage: Lang,
+      expected: Lang): Unit = {
+    val restContext = makeContext(requestLanguages)
+    assert(restContext.selectLanguage(availableLanguages, defaultLanguage) === expected)
+  }
+
+  @Test
+  def basicLanguage(): Unit = {
+    test(
+      requestLanguages = Seq(Lang("en")),
+      availableLanguages = Set(Lang("fr"), Lang("en")),
+      defaultLanguage = Lang("en"),
+      expected = Lang("en"))
+  }
+
+  @Test
+  def defaultFallback(): Unit = {
+    test(
+      requestLanguages = Seq(Lang("zh")),
+      availableLanguages = Set(Lang("fr"), Lang("en")),
+      defaultLanguage = Lang("en"),
+      expected = Lang("en"))
+  }
+
+  @Test
+  def choosePreferred(): Unit = {
+    test(
+      requestLanguages = Seq(Lang("zh"), Lang("fr"), Lang("en")),
+      availableLanguages = Set(Lang("fr"), Lang("en")),
+      defaultLanguage = Lang("en"),
+      expected = Lang("fr"))
+  }
+}
