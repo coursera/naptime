dec50ec3fe8de6664b51166a853c84ab78f752ac
Joshua Newman <josh@coursera.org>
2016-06-29 12:43:54 -0700

    Add strong validator for backwards-compatibility

--- naptime/src/main/scala/org/coursera/naptime/ETag.scala
+++ naptime/src/main/scala/org/coursera/naptime/ETag.scala
@@ -20,28 +20,69 @@ import org.coursera.common.stringkey.StringKeyFormat
 import org.coursera.common.stringkey.StringKeyFormat.Implicits.OrFormat
 
 /**
- * Note: Naptime always uses weak validators because the framework does not guarantee byte-equality
- * between responses.
- *
- * @param weakValidator tag without surrounding `"`s; these are added by Naptime
+ * Resources should always use weak validators because Naptime does not guarantee byte-equality
+ * between responses. See [[ETag.Weak]].
  */
-case class ETag(weakValidator: String) {
-  // See https://tools.ietf.org/html/rfc7232#section-2.3.
-  require(
-    !weakValidator.contains("\"\\"),
-    """Characters '"' and '\' not allowed in ETags""")
-}
+sealed trait ETag
 
 object ETag {
 
-  private[this] val weakValidatorRegex = "W/\"(.*)\"".r
+  def apply(weakValidator: String): Weak = Weak(weakValidator)
 
   implicit val stringKeyFormat: StringKeyFormat[ETag] = {
-    def from(s: String): Option[ETag] = s match {
-      case weakValidatorRegex(tag) => Some(ETag(tag))
-      case _ => None
+    StringKeyFormat.unimplementedFormat[ETag]
+      .orFormat[Weak]
+      .orFormat[Strong]
+  }
+
+  /**
+   * @param weakValidator tag without surrounding `"`s; these are added by Naptime
+   */
+  case class Weak(weakValidator: String) extends ETag {
+    // See https://tools.ietf.org/html/rfc7232#section-2.3.
+    require(
+      !weakValidator.contains("\"\\"),
+      """Characters '"' and '\' not allowed in ETags""")
+  }
+
+  object Weak {
+
+    private[this] val weakValidatorRegex = "W/\"(.*)\"".r
+
+    implicit val stringKeyFormat: StringKeyFormat[Weak] = {
+      def from(s: String): Option[Weak] = s match {
+        case weakValidatorRegex(tag) => Some(Weak(tag))
+        case _ => None
+      }
+      StringKeyFormat.delegateFormat(from, weak => s"""W/"${weak.weakValidator}"""")
     }
-    StringKeyFormat.delegateFormat(from, eTag => s"""W/"${eTag.weakValidator}"""")
+
+  }
+
+  /**
+   * @param strongValidator tag without surrounding `"`s; these are added by Naptime
+   */
+  @deprecated(message = "Use weak validators; see `ETag`'s Scaladoc", since = "0.1.6")
+  case class Strong(strongValidator: String) extends ETag {
+    // See https://tools.ietf.org/html/rfc7232#section-2.3.
+    require(
+      !strongValidator.contains("\"\\"),
+      """Characters '"' and '\' not allowed in ETags""")
+  }
+
+  object Strong {
+
+    private[this] val strongValidatorRegex = "\"(.*)\"".r
+
+    implicit val stringKeyFormat: StringKeyFormat[Strong] = {
+      def from(s: String): Option[Strong] = s match {
+        case strongValidatorRegex(tag) => Some(Strong(tag))
+        case _ => None
+      }
+      StringKeyFormat.delegateFormat(from, strong => s""""${strong.strongValidator}"""")
+    }
+
   }
 
 }
+
