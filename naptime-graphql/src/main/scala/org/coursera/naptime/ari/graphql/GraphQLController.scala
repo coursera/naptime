49ad78bf5aaf0c18f196e6b0a686d336e7d7af18
Bryan Kane <bryan@coursera.org>
2016-10-06 21:00:44 -0700

    Add variables into query execution (#118)
    Properly parse variables while executing queries

--- naptime-graphql/src/main/scala/org/coursera/naptime/ari/graphql/GraphQLController.scala
+++ naptime-graphql/src/main/scala/org/coursera/naptime/ari/graphql/GraphQLController.scala
@@ -62,7 +62,7 @@ class GraphQLController @Inject() (
       case JsString(vars) => Some(parseVariables(vars))
       case obj: JsObject => Some(obj)
       case _ => None
-    }
+    }.getOrElse(Json.obj())
 
     executeQuery(query, request, variables, operation)
   }
@@ -85,7 +85,7 @@ class GraphQLController @Inject() (
   private def executeQuery(
       query: String,
       requestHeader: RequestHeader,
-      variables: Option[JsObject],
+      variables: JsObject,
       operation: Option[String]) = {
 
     QueryParser.parse(query) match {
@@ -104,6 +104,7 @@ class GraphQLController @Inject() (
               graphqlSchemaProvider.schema,
               queryAst,
               context,
+              variables = variables,
               exceptionHandler = exceptionHandler)
               .map(Ok(_))
 
