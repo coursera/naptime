f588f2fd6552b25fa83103cd968c1e6bb50d6ecc
Bryan Kane <bryan@coursera.org>
2016-11-10 14:15:20 -0800

    Add test harness for Filters

---
+++ naptime-graphql/src/test/scala/org/coursera/naptime/ari/graphql/controllers/filters/QueryComplexityFilterTest.scala
@@ -0,0 +1,38 @@
+package org.coursera.naptime.ari.graphql.controllers.filters
+
+import org.junit.Test
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+class QueryComplexityFilterTest extends FilterTest {
+
+  val filter = new QueryComplexityFilter(graphqlSchemaProvider)
+
+  @Test
+  def emptyQuery(): Unit = {
+    val incomingQuery = generateIncomingQuery()
+    val outgoingQuery = run(incomingQuery).futureValue
+    assert(outgoingQuery === baseOutgoingQuery)
+  }
+
+  @Test
+  def complexQuery(): Unit = {
+    val query =
+      """
+        |query {
+        |  CoursesV1Resource {
+        |    getAll(limit: 100000) {
+        |      elements {
+        |        id
+        |      }
+        |    }
+        |  }
+        |}
+      """.stripMargin
+    val incomingQuery = generateIncomingQuery(query)
+    val outgoingQuery = ensureNotPropagated(incomingQuery).futureValue
+
+    assert(outgoingQuery.response.value("error").as[String] === "Query is too complex.")
+    assert(outgoingQuery.response.value("complexity").as[Int] === 200001)
+  }
+}
