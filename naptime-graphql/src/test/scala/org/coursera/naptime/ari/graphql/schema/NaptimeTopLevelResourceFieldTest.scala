cb936e269636ae7c30aaa73a6a99877c0a01e41f
Vishal Kuo <vishalkuo@users.noreply.github.com>
2017-07-10 10:46:32 -0700

    Add dynamic comments to resources (#200)
    This allows us to specify an additional comment for each resource (i.e. the service where it was loaded from) to provide additional data and help with debugging.

--- naptime-graphql/src/test/scala/org/coursera/naptime/ari/graphql/schema/NaptimeTopLevelResourceFieldTest.scala
+++ naptime-graphql/src/test/scala/org/coursera/naptime/ari/graphql/schema/NaptimeTopLevelResourceFieldTest.scala
@@ -64,7 +64,10 @@ class NaptimeTopLevelResourceFieldTest extends AssertionsForJUnit {
       "partner",
       "coursePlatform",
       "arbitraryData")
+    val expectedServiceName = Some("Attributes:\nservice -> myService")
+
     assert(fieldNames === expectedFieldNames)
+    assert(courseResourceField.description === expectedServiceName)
   }
 
   @Test
