f47d00ca5ee33580110740e3e4101074869171c2
Deepkanwal Plaha <dplaha@coursera.org>
2022-03-23 19:57:30 -0400

    update copyright notice

--- naptime-models/src/main/scala/org/coursera/naptime/courier/validation/RequiredMode.scala
+++ naptime-models/src/main/scala/org/coursera/naptime/courier/validation/RequiredMode.scala
@@ -1,16 +1,37 @@
 /*
-   Copyright (c) 2021 Coursera Inc.
+  Copyright (c) 2021 Coursera, Inc.
 
+  This file has been modified by Coursera, Inc. to loosen
+  validation of enums.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+/*
    Copyright (c) 2012 LinkedIn Corp.
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
    You may obtain a copy of the License at
+
        http://www.apache.org/licenses/LICENSE-2.0
 
-   This file has been modified by Coursera Inc. to loosen
-   validation of enums.
- */
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
 
 package org.coursera.naptime.courier.validation
 
