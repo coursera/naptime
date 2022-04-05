57389b53a8b607dff64d54eb7a57f3424574b0f2
Sharon (Sean) Rolel <srolel@coursera.org>
2022-04-05 13:55:33 -0400

    Update SangriaGraphQlSchemaBuilderTest.scala

--- naptime-graphql/src/test/scala/org/coursera/naptime/ari/graphql/SangriaGraphQlSchemaBuilderTest.scala
+++ naptime-graphql/src/test/scala/org/coursera/naptime/ari/graphql/SangriaGraphQlSchemaBuilderTest.scala
@@ -25,7 +25,7 @@ import org.scalatest.mockito.MockitoSugar
 
 class SangriaGraphQlSchemaBuilderTest extends AssertionsForJUnit {
   @Test
-  def badResourceSchema_omitFromGraphqlSchema(): Unit = {
+  def badResourceSchemaOmitFromGraphqlSchema(): Unit = {
     val schemaTypes = Map(
       "org.coursera.naptime.ari.graphql.models.MergedCourse" -> MergedCourse.SCHEMA,
       "org.coursera.naptime.ari.graphql.models.FakeModel" -> RecordWithUnionTypes.SCHEMA,
